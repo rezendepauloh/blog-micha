d2328ef145b8d6327bfa5fa784803b24e47378c0
rezendepauloh <paulo.rezende@outlook.com>
2021-08-06 09:31:20 -0400

    Init

---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+export default {
+  url: 'http://localhost:1337',
+  siteName: 'Michelly Segovia',
+  defaultSlug: 'landing-page',
+};
---
+++ src/styles/theme.ts
@@ -0,0 +1,37 @@
+export const theme = {
+  colors: {
+    primary: '#0A1128',
+    secondary: '#dc143c',
+    white: '#f5f5f6',
+    mediumGray: '#2d2d2d',
+  },
+  font: {
+    family: {
+      default: "'Josefin Sans', 'Open Sans', sans-serif",
+      secondary: "'Montserrat', sans-serif",
+    },
+    sizes: {
+      xsmall: '8rem',
+      small: '1.6rem',
+      medium: '2.4rem',
+      large: '3.2rem',
+      xlarge: '4.0rem',
+      xxlarge: '4.8rem',
+      huge: '5.6rem',
+      xhuge: '6.4rem',
+    },
+  },
+  media: {
+    lteMedium: '(max-width: 768px)',
+  },
+  spacings: {
+    xsmall: '8rem',
+    small: '1.6rem',
+    medium: '2.4rem',
+    large: '3.2rem',
+    xlarge: '4.0rem',
+    xxlarge: '4.8rem',
+    huge: '5.6rem',
+    xhuge: '6.4rem',
+  },
+} as const;

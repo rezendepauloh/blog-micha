982de83a2dbeeb40b9da28de27a36b16112981d7
rezendepauloh <paulo.rezende@outlook.com>
2021-08-18 19:48:01 -0400

    Tentado o mdx do Storybook

--- src/components/CloneMe/stories.tsx
+++ src/components/CloneMe/stories.tsx
@@ -2,9 +2,16 @@ import { Meta, Story } from '@storybook/react/types-6-0';
 import { CloneMe } from '.';
 import { CloneMeProps } from './type';
 
+// import { CloneMeDoc } from './CloneMeDoc';
+
 export default {
   title: 'CloneMe',
   component: CloneMe,
+  // parameters: {
+  //   docs: {
+  //     page: CloneMeDoc,
+  //   },
+  // },
 } as Meta;
 
 export const Template: Story<CloneMeProps> = (args) => {

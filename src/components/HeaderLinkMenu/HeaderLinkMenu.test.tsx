a4cef72e8be2553affffc348f3906156616858a0
Paulo Rezende <paulo.rezende@outlook.com>
2021-09-11 11:30:33 -0400

    Realizado teste do HeaderLinkMenu

--- src/components/HeaderLinkMenu/HeaderLinkMenu.test.tsx
+++ src/components/HeaderLinkMenu/HeaderLinkMenu.test.tsx
@@ -1,88 +1,141 @@
-// import { screen } from '@testing-library/react';
-// import { renderTheme } from 'styles/render-theme';
-// import { HeaderLinkMenu } from '.';
+import { screen } from '@testing-library/react';
+import { renderTheme } from 'styles/render-theme';
+import { HeaderLinkMenu } from '.';
 
-// describe('<HeaderLinkMenu />', () => {
-//   it('should render a link', () => {
-//     renderTheme(
-//       <HeaderLinkMenu link="http://localhost">Children</HeaderLinkMenu>,
-//     );
-//     expect(screen.getByRole('link', { name: 'Children' })).toHaveAttribute(
-//       'target',
-//       '_self',
-//     );
-//   });
+const mock = {
+  id: '1',
+  text: 'MenuLink',
+  link: 'http://localhost/',
+};
 
-//   it('should render a internal link', () => {
-//     renderTheme(<HeaderLinkMenu link="/localhost">Children</HeaderLinkMenu>);
-//     expect(screen.getByRole('link', { name: 'Children' })).toHaveAttribute(
-//       'target',
-//       '_self',
-//     );
-//   });
+//Fazendo um "mock" do useRouter()
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useRouter = jest.spyOn(require('next/router'), 'useRouter');
+export function mockNextUseRouter(pathname: string) {
+  useRouter.mockImplementation(() => ({
+    route: '',
+    basePath: '',
+    pathname,
+    query: {},
+    asPath: '',
+    push: async () => true,
+    replace: async () => true,
+    reload: () => null,
+    back: () => null,
+    prefetch: async () => undefined,
+    beforePopState: () => null,
+    isFallback: false,
+    events: {
+      on: () => null,
+      off: () => null,
+      emit: () => null,
+    },
+  }));
+}
 
-//   it('should render open in a new tab', () => {
-//     renderTheme(
-//       <HeaderLinkMenu link="http://localhost" newTab={true}>
-//         Children
-//       </HeaderLinkMenu>,
-//     );
-//     expect(screen.getByRole('link', { name: 'Children' })).toHaveAttribute(
-//       'target',
-//       '_blank',
-//     );
-//   });
+describe('<HeaderLinkMenu />', () => {
+  mockNextUseRouter('/');
 
-//   it('should render open in a new tab', () => {
-//     const { container } = renderTheme(
-//       <HeaderLinkMenu link="http://localhost" newTab={false}>
-//         Children
-//       </HeaderLinkMenu>,
-//     );
-//     expect(container.firstChild).toMatchInlineSnapshot(`
-//       .c0 {
-//         display: block;
-//         -webkit-text-decoration: none;
-//         text-decoration: none;
-//         text-align: center;
-//         font-size: 1.6rem;
-//         padding: 1.6rem;
-//         color: #f5f5f6;
-//         position: relative;
-//       }
+  it('should render a link', () => {
+    renderTheme(
+      <HeaderLinkMenu id={mock.id} text={mock.text} link={mock.link} />,
+    );
+    expect(screen.getByRole('link', { name: 'MenuLink' })).toHaveAttribute(
+      'target',
+      '_self',
+    );
+  });
 
-//       .c0::after {
-//         content: '';
-//         position: absolute;
-//         bottom: 0.76rem;
-//         left: 50%;
-//         width: 0;
-//         height: 0.2rem;
-//         background: #dc143c;
-//         -webkit-transition: all 300ms ease-in-out;
-//         transition: all 300ms ease-in-out;
-//       }
+  it('should render a internal link', () => {
+    renderTheme(
+      <HeaderLinkMenu
+        id={mock.id}
+        text={mock.text}
+        link="/localhost"
+        newTab={false}
+      />,
+    );
+    expect(screen.getByRole('link', { name: 'MenuLink' })).toHaveAttribute(
+      'target',
+      '_self',
+    );
+  });
 
-//       .c0:hover {
-//         color: #dc143c;
-//         -webkit-transition: all 300ms ease-in-out;
-//         transition: all 300ms ease-in-out;
-//       }
+  it('should render open in a new tab', () => {
+    renderTheme(
+      <HeaderLinkMenu
+        id={mock.id}
+        text="Children"
+        link={mock.link}
+        newTab={true}
+      />,
+    );
+    expect(screen.getByRole('link', { name: 'Children' })).toHaveAttribute(
+      'target',
+      '_blank',
+    );
+  });
 
-//       .c0:hover::after {
-//         left: 25%;
-//         width: 50%;
-//       }
+  it('should match a snapshot', () => {
+    const { container } = renderTheme(
+      <HeaderLinkMenu
+        id={mock.id}
+        text={mock.text}
+        link={mock.link}
+        newTab={false}
+      />,
+    );
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        display: block;
+        -webkit-text-decoration: none;
+        text-decoration: none;
+        text-align: center;
+        font-size: 1.6rem;
+        padding: 1.6rem;
+        color: #f5f5f6;
+        position: relative;
+      }
 
-//       <a
-//         class="c0"
-//         href="http://localhost"
-//         target="_self"
-//       >
-//         Children
-//       </a>
-//     `);
-//   });
-// });
+      .c0::after {
+        content: '';
+        position: absolute;
+        bottom: 0.76rem;
+        left: 50%;
+        width: 0;
+        height: 0.2rem;
+        background: #dc143c;
+        -webkit-transition: all 300ms ease-in-out;
+        transition: all 300ms ease-in-out;
+      }
 
-export {};
+      .c0:hover {
+        color: #dc143c;
+        -webkit-transition: all 300ms ease-in-out;
+        transition: all 300ms ease-in-out;
+      }
+
+      .c0:hover::after {
+        left: 25%;
+        width: 50%;
+      }
+
+      .c0.currentActive {
+        color: #dc143c;
+      }
+
+      .c0.currentActive::after {
+        left: 25%;
+        width: 50%;
+      }
+
+      <a
+        class="c0"
+        href="http://localhost/"
+        target="_self"
+      >
+        MenuLink
+      </a>
+    `);
+  });
+});
